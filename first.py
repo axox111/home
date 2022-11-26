fe38cacf6270f07e50e5fbf5dad9bec56f6a411e
Vladislav <gggwp4@gmail.com>
2022-11-26 12:39:27 +0300

    davai

---
+++ first.py
@@ -0,0 +1,10 @@
+n = 7 #int(input("n = "))
+x = 0
+for i in range(1, n + 1):
+    if i % 3 == 0:
+        x = i ** 2
+    elif i % 3 == 1:
+        x = i
+    else:
+        x = i / 3
+    print(sum(x))
