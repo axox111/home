67c22b660c4b855f69e4dc924fe6d6eb3a9c5eaf
Vladislav <gggwp4@gmail.com>
2022-11-28 20:47:51 +0300

    +296-2

--- 296-2.py
+++ 296-2.py
@@ -2,5 +2,19 @@
 #Поменять в этой последовательности местами:
 #а) наибольший и наименьший члены;
 #б) наибольший и последний члены.
+import random
 
-a = [1, 7, ]
+var_list = random.sample(range(1, 100), 5)
+print("список чисел:", var_list)
+highest = 0 
+lowest = 0
+for i in range(1, len(var_list)):
+    if var_list[i] > var_list[highest]:
+        highest = i
+    if var_list[i] < var_list[lowest]:
+        lowest = i
+var_list[highest], var_list[lowest] = var_list[lowest], var_list[highest]
+print("задание а)", var_list)
+var_list[highest], var_list[lowest] = var_list[lowest], var_list[highest]
+var_list[highest], var_list[-1] = var_list[-1], var_list[highest]
+print("задание б)", var_list)
