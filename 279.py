053e0546dcd3f3e403e457cd505bd980bdc89df0
Vladislav <gggwp4@gmail.com>
2022-11-28 15:43:20 +0300

    коррекция 279, + 296

--- 279.py
+++ 279.py
@@ -2,14 +2,14 @@
 #вычислить (a1 + bn) * (a2 - bn-1) * ... * (an + b1)
 import random
 
-var = int(input("введите количество чисел: "))
+var_number = int(input("введите количество чисел: "))
 max_range = 100
-a = random.sample(range(1, max_range), var)
-b = random.sample(range(1, max_range), var)
+a = random.sample(range(1, max_range), var_number)
+b = random.sample(range(1, max_range), var_number)
 i = 0
 res = 1
 b.reverse()
-while i < var:
+while i < var_number:
     sum = a[i] + b[i]
     res *= sum
     i +=1
