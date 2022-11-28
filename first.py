6bf552370fd1a11267149bfbc8c2211729b495b1
Vladislav <gggwp4@gmail.com>
2022-11-28 12:24:11 +0300

    +279 zadacha

--- first.py
+++ first.py
@@ -1,3 +1,4 @@
+#198 
 n = 7 #int(input("n = "))
 x = 0
 res = 0
@@ -9,4 +10,22 @@ for i in range(1, n + 1):
     else:
         x = i / 3
     res += x
+print(res)
+
+#279Даны действительные числа а1, ..., аn, b1, ..., bn.
+#вычислить (a1 + bn) * (a2 - bn-1) * ... * (an + b1)
+n = 10
+al = []
+bl = []
+s = 0
+ss = 0
+res = 1
+for i in range(1, n + 1):
+    al.append(i)
+    bl.append(i)
+bl.reverse()
+while s < n:
+    ss = al[s] + bl[s]
+    res *= ss
+    s +=1
 print(res)
