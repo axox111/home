5631debc853142cce5990c4f61f1f9e405615797
Vladislav <gggwp4@gmail.com>
2022-11-28 16:30:55 +0300

    corr 279

--- 279.py
+++ 279.py
@@ -8,9 +8,8 @@ a = random.sample(range(1, max_range), var_number)
 b = random.sample(range(1, max_range), var_number)
 i = 0
 res = 1
-b.reverse()
-while i < var_number:
-    sum = a[i] + b[i]
+while i < len(a):
+    sum = a[i] + b[var_number - i - 1]
     res *= sum
     i +=1
 print(a)
