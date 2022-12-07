2a13afc1b6170b985e709b220c88fd951d5f2f54
Vladislav <gggwp4@gmail.com>
2022-12-07 16:51:28 +0300

    334 v2

--- 334.py
+++ 334.py
@@ -8,14 +8,14 @@ res = 0
 for k in range(i, imax + 1):
     for n in range(j, jmax + 1):
         res += 1 / (k + n ** 2)
-print(f"Результат этой агонии - {round(res, 2)}")
+print(f"Результат а агонии - {round(res, 2)}")
 #г)
 i = 1
-imax = 5
+imax = 100
 j = 1
 res = 0
 for k in range(i, imax + 1):
     for n in range(j, i + 1):
         res += 1 / (2 * n + k)
     i += 1
-print(f"Результат этой агонии - {round(res, 2)}")
+print(f"Результат г агонии - {round(res, 2)}")
