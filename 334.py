67cd793dabda9c4b944616c06b8c896fe4d2519b
Vladislav <gggwp4@gmail.com>
2022-12-07 12:49:20 +0300

    +334 za chto

---
+++ 334.py
@@ -0,0 +1,21 @@
+#334 
+#а)
+i = 1
+imax = 100
+j = 1
+jmax = 50
+res = 0
+for k in range(i, imax + 1):
+    for n in range(j, jmax + 1):
+        res += 1 / (k + n ** 2)
+print(f"Результат этой агонии - {round(res, 2)}")
+#г)
+i = 1
+imax = 100
+j = 1
+imax = i
+res = 0
+for k in range(i, imax + 1):
+    for n in range(j, jmax + 1):
+        res += 1 / (2 * n + k)
+print(f"Результат этой агонии - {round(res, 2)}")
