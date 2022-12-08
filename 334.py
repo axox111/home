d982421337640829f2966aa8c178f0ea22b11419
Vladislav <gggwp4@gmail.com>
2022-12-08 09:56:21 +0300

    corr g

--- 334.py
+++ 334.py
@@ -1,21 +1,20 @@
 #334 
 #а)
-i = 1
-imax = 100
-j = 1
-jmax = 50
+i_min_range = 1
+i_max_range = 100
+j_min_range = 1
+j_max_range = 50
 res = 0
-for k in range(i, imax + 1):
-    for n in range(j, jmax + 1):
+for k in range(i_min_range, i_max_range + 1):
+    for n in range(j_min_range, j_max_range + 1):
         res += 1 / (k + n ** 2)
 print(f"Результат а агонии - {round(res, 2)}")
 #г)
-i = 1
-imax = 100
-j = 1
+i_min_range = 1
+i_max_range = 5#100
+j_min_range = 1
 res = 0
-for k in range(i, imax + 1):
-    for n in range(j, i + 1):
-        res += 1 / (2 * n + k)
-    i += 1
+for i in range(i_min_range, i_max_range + 1):
+    for j in range(j_min_range, i + 1):
+        res += 1 / (2 * j + i)
 print(f"Результат г агонии - {round(res, 2)}")
