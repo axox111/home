b6506175c4bad6f9c023aae1b12c4aeed5cebc12
Vladislav <gggwp4@gmail.com>
2022-12-09 10:53:43 +0300

    367 v2

--- 367.py
+++ 367.py
@@ -1,13 +1,16 @@
 #367. Даны целые числа a1, a2, a3. Получить целочисленную матрицу [bijh] = 1,2,3. Для которой biJ = ai— 3aj
 #Нужно получить матрицу 3х3, в которой каждый элемент считается по формуле bij = ai— 3aj. bij - i - это индекс строки, j - индекс столбца
-var1 = int(input("Переменная 1:"))
-var2 = int(input("Переменная 2:"))
-var3 = int(input("Переменная 3:"))
+var1 = 2#int(input("Переменная 1:"))
+var2 = 3#int(input("Переменная 2:"))
+var3 = 4#int(input("Переменная 3:"))
 varList = [var1, var2, var3]
+matrix = [] #1
+arr = []
 size = 3
-matrix = []
 for i in range(size):
+    matrix = [] #2
     for j in range(size):
         matrix.append(varList[i] - 3 * varList[j])
-    print(matrix)
-    matrix =[]
+    arr.insert(i, matrix)
+    matrix = [] #3
+print(arr)
