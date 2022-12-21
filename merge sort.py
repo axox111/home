dfb197067dcd8e1dff77d181a245872cd09fe08d
Vladislav <gggwp4@gmail.com>
2022-12-21 22:31:36 +0300

    +merge sort

--- merge sort.py
+++ merge sort.py
@@ -1,27 +1,29 @@
 def merge_sort(x):
-    first_list = []
-    second_list=[]
-    if len(x) > 1:
-        list_middle = len(x) // 2
-        for i in range(len(x)):
-            if i < list_middle:
-                first_list.append(x[i])
-            else:
-                second_list.append(x[i])
-        merge_sort(first_list)
-        merge_sort(second_list)
-    elif len(x) == 1:
-        i = j = 0
-        final_list = []
-        while i < len(first_list) and j < len(second_list):
-            if first_list[i] < second_list[j]:
-                final_list.append(first_list[i])
-                i += 1
-            else:
-                final_list.append(second_list[j])
-                j += 1
-        
-        
+    if len(x) == 1:
+        return x
+    else:
+        elem = len(x) // 2
+        first_list = merge_sort(x[:elem])
+        second_list = merge_sort(x[elem:])
+        return merge_sorting(first_list, second_list)
 
-a = [10, 7, 9, 4, 6, 3, 1]
+def merge_sorting(x, y):
+    i = j = 0
+    final = []
+    while i < len(x) and j < len(y):
+        if x[i] < y[j]:
+            final.append(x[i])
+            i += 1
+        else:
+            final.append(y[j])
+            j += 1
+    if i < len(x):
+        final += x[i:]
+    if j < len(y):
+        final += y[j:]
+    return final
+            
+a = [10, 7, 9, 4, 6, 3, 1, 2]
+print(a)
 b = merge_sort(a)
+print(b)
