d13a580e7828c798d69a13a6a9ee122c9c6aad77
Vladislav <gggwp4@gmail.com>
2023-01-18 16:28:34 +0300

    data structure

--- OOP/Stack Queue.py
+++ OOP/Stack Queue.py
@@ -13,12 +13,14 @@ class Stack:
         if self.size == -1:
             print('Список пуст. Нечего удалять')
         else:
+            poped = self.values[-1]
             self.values = self.values[0:self.size]
             self.size -= 1
+            print(f"Удаляем {poped}")
         
-    def public(self):
+    def getSize(self):
         volume = self.size + 1
-        return volume
+        print(volume)
         
         
 class Queue:
@@ -29,17 +31,18 @@ class Queue:
         self.size = -1
 
     def push(self, val):
-       self.values = [val] + self.values
+       self.values = self.values + [val]
        self.size += 1
        
     def pop(self):
         if self.size == -1:
             print('Список пуст. Нечего удалять')
         else:
+            poped = self.values[-1]
             self.values = self.values[0:self.size]
             self.size -= 1
+            print(f"Удаляем {poped}")
         
-    def public(self):
+    def getSize(self):
         volume = self.size + 1
-        return volume
-
+        print(volume)
